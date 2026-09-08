5ca4561a8feb9f65eead9cad75d35db31e1f1b8e
Selim <selim@ownweb.fr>
2026-09-08 10:32:07 +0200

    script_speed

--- Assets/Ressources/script1.cs
+++ Assets/Ressources/script1.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class script1 : MonoBehaviour
 {
+
+    public float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,6 @@ public class script1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position += new Vector3(0,0, this.speed);
     }
 }
